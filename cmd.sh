d92eb1d5e44a01450e05f27ce4260eede77e1051
Shima Rastegarnia <64853244+srastegarnia@users.noreply.github.com>
2020-06-12 03:53:01 -0400

    Add files via upload
    Batch file

---
+++ cmd.sh
@@ -0,0 +1,26 @@
+jupyter notebook
+echo $SHELL
+ls
+ls -FS
+ls -F
+ls -F /
+ls -s
+ls -S
+ls -l
+ls -h
+ls -lh
+ls -lh
+ls -Fa
+ls -F
+man ls
+mv -i
+rm -r -i
+$PATH
+cd -
+pwd
+cd /mnt/c/Users/sh_ra/OneDrive/Desktop/Brainhack_2020/data-shell/data-shell/data/
+mkdir thesis
+cd thesis/
+nano draft.text
+touch my_file.txt
+history
